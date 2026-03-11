a654b71303b0f7f504cb0690ec02f05211128c25
CDalianis <christos_dl@hotmail.com>
2026-03-11 19:10:15 +0200

    fix: user insert dto

--- src/main/java/gr/aueb/cf/eduapp/dto/UserInsertDTO.java
+++ src/main/java/gr/aueb/cf/eduapp/dto/UserInsertDTO.java
@@ -1,6 +1,5 @@
 package gr.aueb.cf.eduapp.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -15,7 +14,7 @@ public record UserInsertDTO(
         @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$")
         String password,
 
-        @NotBlank
+        @NotNull
         Long roleId
 ) {
 }
